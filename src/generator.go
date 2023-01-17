fa861d46f09d94e6b964e3cbeb362b11eb403b59
Daniel Bos <daniel.bos@sap.com>
2023-01-17 15:43:20 +0800

    use llir/llvm to generate ir

--- src/generator.go
+++ src/generator.go
@@ -4,92 +4,116 @@ import (
 	"fmt"
 	"io"
 	"runtime"
+
+	"github.com/llir/llvm/ir"
+	"github.com/llir/llvm/ir/constant"
+	"github.com/llir/llvm/ir/types"
+	"github.com/llir/llvm/ir/value"
 )
 
 func generateIR(writer io.Writer, root Node) {
 	g := &generator{
-		out:   writer,
-		ssaid: 1,
-		conid: 0,
+		currentModule: ir.NewModule(),
+		currentBlock:  nil,
+		currentValue:  nil,
+		stdlib:        make(map[string]*ir.Func),
+		strings:       make(map[string]*ir.Global),
 	}
 
 	g.Generate(root)
+
+	fmt.Fprintln(writer, g.currentModule)
 }
 
 type generator struct {
-	out   io.Writer
-	ssaid int
-	conid int
+	currentModule *ir.Module
+	currentBlock  *ir.Block
+	currentValue  value.Value
+	stdlib        map[string]*ir.Func
+	strings       map[string]*ir.Global
 }
 
 var _ Visitor = (*generator)(nil)
 
 func (g *generator) Generate(root Node) {
-	fmt.Fprintln(g.out, `target datalayout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128"`)
-	fmt.Fprintf(g.out, "target triple = \"%v\"\n", g.getTargetTriple())
+	g.currentModule.TargetTriple = g.getTargetTriple()
+	g.generateStdlib()
 
 	root.Visit(g)
 }
 
 func (g *generator) VisitModule(n *Module) {
-	format := "%d\\00" // this nonsense is needed to keep govet quiet
-	fmt.Fprintln(g.out, `@format = internal constant [3 x i8] c"`+format+`"`)
-	fmt.Fprintln(g.out, `define dso_local i32 @main() {`)
+	main := g.currentModule.NewFunc("main", types.I32)
+	g.stdlib["main"] = main
+
+	g.currentBlock = main.NewBlock("entry")
 
 	for _, stmt := range n.stmts {
 		stmt.Visit(g)
 	}
 
-	fmt.Fprintln(g.out, `  ret i32 0`)
-	fmt.Fprintln(g.out, `}`)
-	fmt.Fprintln(g.out, `declare dso_local i32 @puts(i8*)`)
-	fmt.Fprintln(g.out, `declare dso_local i32 @rand()`)
-	fmt.Fprintln(g.out, `declare dso_local i32 @sprintf(i8*, i8*, ...)`)
+	g.currentBlock.NewRet(constant.NewInt(types.I32, 0))
 }
 
 func (g *generator) VisitPrintStmt(n *PrintStmt) {
 	for _, arg := range n.args {
 		arg.Visit(g)
-		number := fmt.Sprintf("%%%d", g.ssaid-1)
+		number := g.currentValue
 
-		buf := g.assign("alloca [318 x i8]")
-		bufptr := g.assign("getelementptr inbounds [318 x i8], [318 x i8]* %v, i64 0, i64 0", buf)
-		formatptr := g.assign(`getelementptr inbounds [3 x i8], [3 x i8]* @format, i64 0, i64 0`)
-		g.assign(`call i32 (i8*, i8*, ...) @sprintf(i8* %v, i8* %v, i32 %v)`, bufptr, formatptr, number)
-		g.assign(`call i32 @puts(i8* %v)`, bufptr)
+		buf := g.currentBlock.NewAlloca(types.NewArray(318, types.I8))
+		bufptr := g.currentBlock.NewGetElementPtr(buf.ElemType, buf, constant.NewInt(types.I32, 0), constant.NewInt(types.I32, 0))
+
+		format := g.intern("%d\000")
+		formatptr := g.currentBlock.NewGetElementPtr(format.ContentType, format, constant.NewInt(types.I32, 0), constant.NewInt(types.I32, 0))
+
+		g.currentBlock.NewCall(g.stdlib["sprintf"], bufptr, formatptr, number)
+		g.currentBlock.NewCall(g.stdlib["puts"], bufptr)
 	}
 }
 
 func (g *generator) VisitBinaryExpr(n *BinaryExpr) {
 	n.args[0].Visit(g)
-	left := fmt.Sprintf("%%%d", g.ssaid-1)
+	left := g.currentValue
 	n.args[1].Visit(g)
-	right := fmt.Sprintf("%%%d", g.ssaid-1)
+	right := g.currentValue
+
 	switch n.token.Type {
 	case TokenMinus:
-		g.assign("sub i32 %v, %v", left, right)
+		g.currentValue = g.currentBlock.NewSub(left, right)
 	case TokenPlus:
-		g.assign("add i32 %v, %v", left, right)
+		g.currentValue = g.currentBlock.NewAdd(left, right)
 	case TokenStar:
-		g.assign("mul i32 %v, %v", left, right)
+		g.currentValue = g.currentBlock.NewMul(left, right)
 	case TokenSlash:
-		g.assign("udiv i32 %v, %v", left, right)
+		g.currentValue = g.currentBlock.NewSDiv(left, right)
 	}
 }
 
 func (g *generator) VisitNumberExpr(n *NumberExpr) {
-	val := g.assign("alloca i32")
-	fmt.Fprintf(g.out, "store i32 %v, i32* %v\n", n.token.Number, val)
-	g.assign("load i32, i32* %v", val)
+	g.currentValue = constant.NewInt(types.I32, int64(n.token.Number))
 }
 
-func (g *generator) assign(format string, args ...any) string {
-	id := g.ssaid
-	g.ssaid++
+func (g *generator) generateStdlib() {
+	g.stdlib["puts"] = g.currentModule.NewFunc("puts", types.I32,
+		ir.NewParam("str", types.I8Ptr))
+	g.stdlib["rand"] = g.currentModule.NewFunc("rand", types.I32)
 
-	fmt.Fprintf(g.out, "%%%d = %v\n", id, fmt.Sprintf(format, args...))
+	sprintf := g.currentModule.NewFunc("sprintf", types.I32,
+		ir.NewParam("buf", types.I8Ptr),
+		ir.NewParam("format", types.I8Ptr))
+	sprintf.Sig.Variadic = true
+	g.stdlib["sprintf"] = sprintf
+}
 
-	return fmt.Sprintf("%%%d", id)
+func (g *generator) intern(s string) *ir.Global {
+	if v, ok := g.strings[s]; ok {
+		return v
+	}
+
+	def := g.currentModule.NewGlobalDef("", constant.NewCharArrayFromString(s))
+	g.strings[s] = def
+
+	return def
 }
 
 func (g *generator) getTargetTriple() string {
