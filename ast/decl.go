7c349ac468ddf24782df1b309ce3e021c17ec637
Daniel Bos <daniel.bos@sap.com>
2023-02-13 10:58:02 +0800

    add type, const and var to symbol table

--- ast/decl.go
+++ ast/decl.go
@@ -12,6 +12,8 @@ const (
 	KindUndefined Kind = iota
 	KindVar
 	KindConst
+	KindType
+	KindModule
 )
 
 func (k Kind) String() string {
@@ -22,12 +24,38 @@ func (k Kind) String() string {
 		return "variable"
 	case KindConst:
 		return "constant"
+	case KindType:
+		return "type"
+	case KindModule:
+		return "module"
 	default:
 		return fmt.Sprintf("undefined=%d", int(k))
 	}
 }
 
+type Symbol interface {
+	Kind() Kind
+	Type() Type
+}
+
+func GetBuiltinSymbols() map[string]Symbol {
+	return map[string]Symbol{
+		"INTEGER": newBuiltinTypeDecl("INTEGER", TypeInt64),
+		"REAL":    newBuiltinTypeDecl("REAL", TypeFloat64),
+		"BOOLEAN": newBuiltinTypeDecl("BOOLEAN", TypeBoolean),
+		"CHAR":    newBuiltinTypeDecl("CHAR", TypeChar),
+		"SET":     newBuiltinTypeDecl("SET", TypeSet),
+	}
+}
+
 // ----- ConstDecl ------------------------------------------------------------
+
+func NewConstDecl(t token.Token, e Expr) *ConstDecl {
+	return &ConstDecl{
+		token: t,
+		expr:  e,
+	}
+}
 
 type ConstDecl struct {
 	token token.Token
@@ -36,11 +64,10 @@ type ConstDecl struct {
 	value *Value
 }
 
-func NewConstDecl(t token.Token, e Expr) *ConstDecl {
-	return &ConstDecl{
-		token: t,
-		expr:  e,
-	}
+var _ Symbol = (*ConstDecl)(nil)
+
+func (d *ConstDecl) Kind() Kind {
+	return KindConst
 }
 
 func (d *ConstDecl) Token() token.Token {
@@ -66,17 +93,37 @@ func (d *ConstDecl) Update(t Type, v *Value) {
 
 // ----- TypeDecl -------------------------------------------------------------
 
+func NewTypeDecl(token, typeToken token.Token) *TypeDecl {
+	return &TypeDecl{
+		token:     token,
+		typeToken: typeToken,
+	}
+}
+
+func newBuiltinTypeDecl(name string, typ Type) *TypeDecl {
+	t := token.Token{
+		Type: token.TokenIdent,
+		File: "<builtin>",
+		Text: name,
+	}
+
+	return &TypeDecl{
+		token:     t,
+		typeToken: t,
+		typ:       typ,
+	}
+}
+
 type TypeDecl struct {
 	token     token.Token
 	typeToken token.Token
 	typ       Type
 }
 
-func NewTypeDecl(token, typeToken token.Token) *TypeDecl {
-	return &TypeDecl{
-		token:     token,
-		typeToken: typeToken,
-	}
+var _ Symbol = (*TypeDecl)(nil)
+
+func (d *TypeDecl) Kind() Kind {
+	return KindType
 }
 
 func (d *TypeDecl) Token() token.Token {
@@ -97,17 +144,23 @@ func (d *TypeDecl) Update(t Type) {
 
 // ----- VarDecl --------------------------------------------------------------
 
+func NewVarDecl(token, typeToken token.Token) *VarDecl {
+	return &VarDecl{
+		token:     token,
+		typeToken: typeToken,
+	}
+}
+
 type VarDecl struct {
 	token     token.Token
 	typeToken token.Token
 	typ       Type
 }
 
-func NewVarDecl(token, typeToken token.Token) *VarDecl {
-	return &VarDecl{
-		token:     token,
-		typeToken: typeToken,
-	}
+var _ Symbol = (*VarDecl)(nil)
+
+func (d *VarDecl) Kind() Kind {
+	return KindVar
 }
 
 func (d *VarDecl) Token() token.Token {
